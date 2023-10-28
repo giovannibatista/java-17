8ed89c98e4457907dc5dcc35c4e011612187fed2
Giovanni Carlos <giovanni_carlos@sicredi.com.br>
2023-10-28 16:12:32 -0300

    first commit

---
+++ src/com/personal/Triangle.java
@@ -0,0 +1,17 @@
+package com.personal;
+
+public class Triangle implements Shape{
+
+	double base;
+	double height;
+
+	public Triangle(double base, double height) {
+		this.base = base;
+		this.height = height;
+	}
+
+	@Override
+	public double caculateArea() {
+		return (base * height) / 2;
+	}
+}
